9944299fcd4ca5de8f5d3fdba728c9aef10d2075
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-03-23 19:18:16 -0500

    Starting to learn golang after C and C++

---
+++ basic-skills/01-go-in-x-y.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	var message string = "Hola mundo"
+	easyMessage := "Hola mundo usando :="
+	fmt.Println(message)
+	fmt.Println(easyMessage)
+
+	// Comments
+	a := 10.
+	var b float64 = 3
+	fmt.Println(a / b)
+	
+	var c int = 10
+	d := 3
+	fmt.Println(c / d)
+
+	x := true
+	y := false
+
+	fmt.Println(x || y)
+	fmt.Println(x && y)
+	fmt.Println(!x)
+
+	privada()
+	Publica()
+	imprimirHola()
+}
+
+func privada() {
+	fmt.Println("Ejecuta logica que no necesita ser exportada")
+}
+
+// Publica - This function can be used on other packages
+func Publica() {
+	fmt.Println("Ejecuta logica que quiero exportar")
+}
+
+func imprimirHola() {
+	defer fmt.Println("Mundo")
+	fmt.Println("Hola")
+}
